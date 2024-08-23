90ef79d5e063a62512a951473b19997a3e707bb2
antoniomarino024@gmail.com <antoniomarino024@gmail.com>
2024-08-23 15:50:26 +0200

    configuration server,db and router

---
+++ src/routers/orderRouter.ts
@@ -0,0 +1,16 @@
+import express, { Response, Request } from "express";
+import { config } from "dotenv";
+import { open } from 'sqlite';
+import sqlite3 from 'sqlite3';
+import { authenticateToken } from "../middleware/authenticateToken";
+
+config()
+
+export const routerOrder = express.Router()
+
+async function getDb() {
+    return open({
+      filename: 'mydb.sqlite',
+      driver: sqlite3.Database
+    });
+  };
