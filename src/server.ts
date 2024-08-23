406a77effa133f17fb4f26227dc2dc64af96c413
antoniomarino024@gmail.com <antoniomarino024@gmail.com>
2024-08-23 16:26:44 +0200

    configuration nodemon and create tables

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { routerAuth } from './routers/authRouter';
 import { routerProduct } from './routers/productRouter';
 import { routerOrder } from './routers/orderRouter';
 import { routerCart } from './routers/cartRouter';
+import { getDb } from './db';
 
 
 config();
@@ -14,6 +15,62 @@ const app = express();
 
 // Middleware per parsare i dati JSON
 app.use(express.json());
+
+// Funzione per inizializzare il database
+async function initializeDatabase() {
+  const db = await getDb()
+
+  await db.exec(`
+   -- Creazione della tabella degli utenti
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS auth (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userId INTEGER NOT NULL,
+    Token TEXT NOT NULL,
+    FOREIGN KEY(userId) REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS products(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userId INTEGER NOT NULL,
+    nameProduct TEXT NOT NULL,
+    price TEXT NOT NULL,
+    img TEXT NOT NULL,
+    category TEXT NOT NULL,
+    FOREIGN KEY(userId) REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS orders(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userId INTEGER NOT NULL,
+    Token TEXT NOT NULL,
+    status TEXT NOT NULL,
+    date TEXT NOT NULL,
+    FOREIGN KEY(userId) REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS cart(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userId INTEGER NOT NULL,
+    productId INTEGER NOT NULL,
+    FOREIGN KEY(userId) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY(productId) REFERENCES products(id) ON DELETE CASCADE
+);
+
+  `);
+}
+
+// Inizializza il database
+initializeDatabase()
 
 
 // Configura le route
