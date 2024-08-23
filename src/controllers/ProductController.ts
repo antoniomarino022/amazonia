d15aee5d94b38807b4d8a74fe0a8679931f25d04
Gabriele Lo Re <gabrolr70@gmail.com>
2024-08-23 17:41:50 +0200

    crud product controller

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -17,4 +17,47 @@ export class ProductController{
         }
         this.products = [...this.products, product];
     }
+    removeProduct(referenceKeyUser:string, primaryKeyProduct: string)
+    {
+        if(!this.userController.getUser(referenceKeyUser)?.admin)
+        {
+            console.log("Non sei admin per mettere i prodotti");
+            return false;
+        }
+        this.products = this.products.filter(function(product){
+                if(product.primaryKeyProduct==primaryKeyProduct)
+                {
+                    return false;
+                }
+                return true;
+        });
+    }
+    updateProduct(
+        primaryKeyProduct: string,
+        name: string,
+        description: string,
+        price: Number,
+        img: string,
+        category: string)
+    {
+
+        this.products = this.products.map(function(product)
+        {
+            if(product.primaryKeyProduct==primaryKeyProduct)
+            {
+                return {...product, name, description, price, img, category}
+            }
+            return product;
+        })
+    }
+    getProductsByCategory(category: string)
+    {
+        return this.products.filter(function(product){
+            if(product.category == category)
+            {
+                return true;
+            }
+            return false;
+        });
+    }
 }
