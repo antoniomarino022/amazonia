99fa407618109b1eba3deed139fcb29aac94c1ee
antoniomarino024@gmail.com <antoniomarino024@gmail.com>
2024-08-27 15:14:36 +0200

    add password-validator

---
+++ src/middleware/passwordValidator.ts
@@ -0,0 +1,16 @@
+
+import PasswordValidator from 'password-validator';
+
+const schema = new PasswordValidator();
+schema
+  .is().min(8)
+  .is().max(100)
+  .has().uppercase()
+  .has().lowercase()
+  .has().digits(1)
+  .has().not().spaces()
+  .is().not().oneOf(['Passw0rd', 'Password123']);
+
+export default schema
+
+
