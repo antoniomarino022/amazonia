5324b7126dfd557afb5ed8b1c89e9aef12aada0c
Gabriele Lo Re <gabrolr70@gmail.com>
2024-08-28 17:38:18 +0200

    fixed anto shitty code

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -131,40 +131,8 @@ routerUser.post('', async (req: Request, res: Response) => {
     } else {
       logger.error('Errore sconosciuto durante la registrazione utente', { error: err });
       return res.status(500).json({ message: 'Errore sconosciuto', err });
-
-    const existingUser = await db.get('SELECT * FROM users WHERE email = ?', [email]);
-
-    if (existingUser) {
-      logger.warn(`Tentativo di registrazione con email già esistente: ${email}`);
-      return res.status(400).json(responses.userExists);
     }
-    if(whitelist.includes(email))
-    {
-      const result = await db.run("INSERT INTO users (username, email, password, is_admin) VALUES (?, ?, ?, ?)", [username, email, passwordHash, 1]);
-
-      if (result.changes && result.changes > 0) {
-        const user = await db.get('SELECT * FROM users WHERE id = ?', [result.lastID]);
-        logger.info(`Nuovo admin registrato: ${username} (${email})`);
-        return res.status(200).json({ message: 'Utente registrato', user: user });
-      } else {
-        logger.error(`Errore durante la registrazione di ${username} (${email})`);
-        return res.status(500).json({ message: "Si è verificato un errore durante la registrazione" });
-      }
-    }
-    else
-    {
-      return res.status(401).json({ message: "Non è un email admin" });
-    }
-  } catch (err) {
-      if(err instanceof Error){
-        logger.error(`Errore JavaScript: ${err.message}`);
-        return res.status(500).json({'message':'errore standar di js','errore':err.message})
-      }else{
-        logger.error(`Errore sconosciuto: ${err}`);
-        return res.status(500).json({ message: 'Errore sconosciuto', err });
-      }
-    } 
-  });
+  }});
 
 
 
