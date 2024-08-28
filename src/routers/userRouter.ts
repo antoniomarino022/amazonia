921ed240f0c3853c7389edb037466555e7895ea1
Gabriele Lo Re <gabrolr70@gmail.com>
2024-08-28 17:55:45 +0200

    added admin methods

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -26,7 +26,6 @@ async function getDb() {
     username: string;
     email: string;
     password: string;
-    is_admin: boolean;
   }
   
   
@@ -63,19 +62,13 @@ async function getDb() {
     }
   
   })
-  
-
-
-
-
-
 
 // register user
 routerUser.post('', async (req: Request, res: Response) => {
   try {
-    const { username, email, password, is_admin }: UserRequestBody = req.body;
+    const { username, email, password }: UserRequestBody = req.body;
 
-    if (!username || !email || !password || is_admin === undefined) {
+    if (!username || !email || !password ) {
       logger.warn('Parametri mancanti nella richiesta', { body: req.body });
       return res.status(400).json({ message: 'Alcuni parametri sono mancanti o non validi' });
     }
@@ -112,11 +105,11 @@ routerUser.post('', async (req: Request, res: Response) => {
 
     const result = await db.run(
       "INSERT INTO users (username, email, password, is_admin) VALUES (?, ?, ?, ?)",
-      [username, email, passwordHash, is_admin]
+      [username, email, passwordHash, 0]
     );
 
     if (result.changes && result.changes > 0) {
-      logger.info('Nuovo utente registrato con successo', { username, email, is_admin });
+      logger.info('Nuovo utente registrato con successo', { username, email, isAdmin: "false" });
       const user = await db.get('SELECT * FROM users WHERE id = ?', [result.lastID]);
       return res.status(201).json({ message: 'Utente registrato', user }); 
     } else {
@@ -133,8 +126,6 @@ routerUser.post('', async (req: Request, res: Response) => {
       return res.status(500).json({ message: 'Errore sconosciuto', err });
     }
   }});
-
-
 
 // update user
 routerUser.put('/update/:id', authenticateToken, async (req: Request, res: Response) => {
@@ -202,8 +193,208 @@ routerUser.put('/update/:id', authenticateToken, async (req: Request, res: Respo
   }
 });
 
+// delete user
+routerUser.delete('/:id', async (req: Request, res: Response) => {
+  logger.info("Tentativo di eliminare l'utente ricevuto", { 'id': req.params.id });
+
+  try {
+    const db = await getDb();
+    const { id } = req.params;
+
+    if (!id) {
+      logger.warn('ID utente mancante nella richiesta');
+      return res.status(400).json({ message: 'ID richiesto' });
+    }
+
+    const existingUser = await db.get('SELECT * FROM users WHERE id = ?', [id]);
+
+    if (!existingUser) {
+      logger.warn('Utente non trovato', { id });
+      return res.status(404).json(responses.notUserExists);  // Usa 404 per "Not Found"
+    }
+
+    const result = await db.run('DELETE FROM users WHERE id = ?', [id]);
+
+    if (result.changes && result.changes > 0) {
+      logger.info(`Utente eliminato con successo`, { id });
+      return res.status(204).send();  // Usa 204 per "No Content"
+    } else {
+      logger.error('Utente non eliminato', { id });
+      return res.status(400).json({ message: 'Utente non eliminato' });
+    }
+
+  } catch (err) {
+    if (err instanceof Error) {
+      logger.error(`Errore JavaScript: ${err.message}`);
+      return res.status(500).json({ message: 'Errore standard di JavaScript', errore: err.message });
+    } else {
+      logger.error(`Errore sconosciuto: ${err}`);
+      return res.status(500).json({ message: 'Errore sconosciuto', err });
+    }
+  }
+});
+
+//ADMIN
+
+// register user as admin
+routerUser.post('/admin', async (req: Request, res: Response) => {
+  try {
+    const { username, email, password}: UserRequestBody = req.body;
+
+    if (!username || !email || !password ) {
+      logger.warn('Parametri mancanti nella richiesta', { body: req.body });
+      return res.status(400).json({ message: 'Alcuni parametri sono mancanti o non validi' });
+    }
+
+    logger.info('Tentativo di registrazione utente ricevuto', { username, email });
+
+   
+    if (!validateUsername(username)) {
+      logger.warn('Username non valido', { username });
+      return res.status(400).json(responses.invalidUsername);
+    }
+
+    if (!validateEmail(email)) {
+      logger.warn('Email non valida', { email });
+      return res.status(400).json(responses.invalidEmail);
+    }
+
+    if (!validatePassword(password)) {
+      logger.warn('Password non valida');
+      return res.status(400).json(responses.invalidPassword);
+    }
+    if(!whitelist.includes(email))
+    {
+      logger.warn('Tentativo di registrazione admin senza email giusta');
+      return res.status(401).json(responses.invalidEmail);
+    }
+    const db = await getDb();
+    const existingUser = await db.get('SELECT * FROM users WHERE email = ?', [email]);
+
+    if (existingUser) {
+      logger.warn('Tentativo di registrazione con email già esistente', { email });
+      return res.status(409).json(responses.userExists);  
+    }
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
 
 
+    const result = await db.run(
+      "INSERT INTO users (username, email, password, is_admin) VALUES (?, ?, ?, ?)",
+      [username, email, passwordHash, 1]
+    );
+
+    if (result.changes && result.changes > 0) {
+      logger.info('Nuovo utente registrato con successo', { username, email, isAdmin: "true" });
+      const user = await db.get('SELECT * FROM users WHERE id = ?', [result.lastID]);
+      return res.status(201).json({ message: 'Utente registrato', user }); 
+    } else {
+      logger.error('Errore durante la registrazione utente', { username, email });
+      return res.status(500).json({ message: "Si è verificato un errore durante la registrazione" });
+    }
+
+  } catch (err) {
+    if (err instanceof Error) {
+      logger.error('Errore JavaScript durante la registrazione utente', { error: err.message });
+      return res.status(500).json({ message: 'Errore interno al server', errore: err.message });
+    } else {
+      logger.error('Errore sconosciuto durante la registrazione utente', { error: err });
+      return res.status(500).json({ message: 'Errore sconosciuto', err });
+    }
+  }});
+
+// revoke admin permissions
+routerUser.put('/admin/:id', authenticateToken, async (req: Request, res: Response) => {
+  logger.info("Tentativo di modificare l'admin ricevuto", { username: req.body.username });
+
+  try {
+    const db = await getDb();
+    const { id } = req.params;
+
+    
+    if (!id) {
+      logger.warn('ID utente mancante nella richiesta', { id: req.params });
+      return res.status(400).json({ message: 'ID utente mancante o non valido' });
+    }
+
+    const user = await db.get('SELECT * FROM users WHERE id = ?', [id]);
+
+    if (!user) {
+      logger.warn('Utente non trovato', { id });
+      return res.status(404).json(responses.notUserExists); 
+    }
+    if(user.is_admin==0)
+      {
+        logger.warn('L\'utente non è un admin', { id });
+        return res.status(400).json(responses.notAdmin); 
+      }
+    
+    const result = await db.run('UPDATE users SET is_admin = ? WHERE id = ?', [0, id]);
+
+    if (result.changes && result.changes > 0) {
+      logger.info(`Utente modificato con successo`, { id });
+      return res.status(200).json({ message: "Admin modificato con successo" });
+    } else {
+      logger.error("Nessuna modifica applicata all'utente", { id });
+      return res.status(304).json({ message: "Nessuna modifica applicata all'admin" }); // Usa 304 per "Not Modified"
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      logger.error(`Errore JavaScript: ${err.message}`);
+      return res.status(500).json({ message: 'Errore interno al server', errore: err.message });
+    } else {
+      logger.error(`Errore sconosciuto: ${err}`);
+      return res.status(500).json({ message: 'Errore sconosciuto', err });
+    }
+  }
+});
+
+// grant admin permissions
+routerUser.put('/admin/:id', authenticateToken, async (req: Request, res: Response) => {
+  logger.info("Tentativo di modificare l'admin ricevuto", { username: req.body.username });
+
+  try {
+    const db = await getDb();
+    const { id } = req.params;
+
+    
+    if (!id) {
+      logger.warn('ID utente mancante nella richiesta', { id: req.params });
+      return res.status(400).json({ message: 'ID utente mancante o non valido' });
+    }
+
+    const user = await db.get('SELECT * FROM users WHERE id = ?', [id]);
+
+    if (!user) {
+      logger.warn('Utente non trovato', { id });
+      return res.status(404).json(responses.notUserExists); 
+    }
+    if(user.is_admin==1)
+      {
+        logger.warn('L\'utente è un admin già!', { id });
+        return res.status(400).json(responses.alreadyAdmin); 
+      }
+    
+    const result = await db.run('UPDATE users SET is_admin = ? WHERE id = ?', [1, id]);
+
+    if (result.changes && result.changes > 0) {
+      logger.info(`Utente modificato con successo`, { id });
+      return res.status(200).json({ message: "Admin modificato con successo" });
+    } else {
+      logger.error("Nessuna modifica applicata all'utente", { id });
+      return res.status(304).json({ message: "Nessuna modifica applicata all'admin" }); // Usa 304 per "Not Modified"
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      logger.error(`Errore JavaScript: ${err.message}`);
+      return res.status(500).json({ message: 'Errore interno al server', errore: err.message });
+    } else {
+      logger.error(`Errore sconosciuto: ${err}`);
+      return res.status(500).json({ message: 'Errore sconosciuto', err });
+    }
+  }
+});
 
 // delete user
 routerUser.delete('/:id', async (req: Request, res: Response) => {
@@ -246,7 +437,47 @@ routerUser.delete('/:id', async (req: Request, res: Response) => {
   }
 });
 
+// delete admin
+routerUser.delete('/admin/:id', async (req: Request, res: Response) => {
+  logger.info("Tentativo di eliminare l'utente admin ricevuto", { 'id': req.params.id });
 
+  try {
+    const db = await getDb();
+    const { id } = req.params;
 
+    if (!id) {
+      logger.warn('ID utente mancante nella richiesta');
+      return res.status(400).json({ message: 'ID richiesto' });
+    }
 
+    const existingUser = await db.get('SELECT * FROM users WHERE id = ?', [id]);
 
+    if (!existingUser) {
+      logger.warn('Utente non trovato', { id });
+      return res.status(404).json(responses.notUserExists);  // Usa 404 per "Not Found"
+    }
+    if(existingUser.is_admin==0)
+    {
+      logger.warn('L\'utente non è un admin', { id });
+      return res.status(400).json(responses.notAdmin); 
+    }
+    const result = await db.run('DELETE FROM users WHERE id = ?', [id]);
+
+    if (result.changes && result.changes > 0) {
+      logger.info(`Admin eliminato con successo`, { id });
+      return res.status(204).send();  // Usa 204 per "No Content"
+    } else {
+      logger.error('Admin non eliminato', { id });
+      return res.status(400).json({ message: 'Admin non eliminato' });
+    }
+
+  } catch (err) {
+    if (err instanceof Error) {
+      logger.error(`Errore JavaScript: ${err.message}`);
+      return res.status(500).json({ message: 'Errore standard di JavaScript', errore: err.message });
+    } else {
+      logger.error(`Errore sconosciuto: ${err}`);
+      return res.status(500).json({ message: 'Errore sconosciuto', err });
+    }
+  }
+});
